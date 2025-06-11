816b35a18c01ed60b9e74c9290b5ec21ad763a2e
Vishal Bari <barivishal@gmail.com>
2025-06-11 12:16:45 +0530

    Initial Commit for push project to GitHub

---
+++ GitProj01/src/main/java/com/nt/main/App.java
@@ -0,0 +1,13 @@
+package com.nt.main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
